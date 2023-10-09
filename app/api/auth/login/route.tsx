c64b601ee54625598999ef9dff1d70033981bb67
Josu <cyrusnimda@gmail.com>
2023-10-09 21:44:01 +0100

    Added bcrypt

--- app/api/auth/login/route.tsx
+++ app/api/auth/login/route.tsx
@@ -1,19 +1,39 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+const bcrypt = require('bcrypt');
 
 export async function POST(request: NextRequest) {
     try {
         const loginRequest = await request.json()
-        if(loginRequest.email == "aaa" && loginRequest.password == "bbb") {
-            return NextResponse.json(
-                {message: "This works"}
-            )
-        } else {
+
+        const hash = loginRequest.password;
+        const user = await prisma.user.findUnique({
+            where: {
+              username: loginRequest.email,
+            },
+        })
+
+        if(!user){
             return NextResponse.json(
                 {message: "Unauthorized"},
                 {status: 401}
             )
         }
 
+        const match = await bcrypt.compare(loginRequest.password, user.password);
+
+        if(!match) {
+            return NextResponse.json(
+                {message: "Unauthorized"},
+                {status: 401}
+            )
+        } 
+
+        return NextResponse.json(
+            {message: "This works"}
+        )
         
     } catch (error: any) {
         return NextResponse.json(
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const UsersPage = () => {
   return (
     <div>UsersPage</div>
